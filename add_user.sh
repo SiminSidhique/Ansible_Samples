c258ee4eb75d03fe720d0ca0efd2c08f29e87d03
SiminSidhique <93574407+SiminSidhique@users.noreply.github.com>
2021-12-10 10:56:52 +0800

    My first codes for PF

---
+++ add_user.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+echo "hello"
+# for i in jen william matt sam anne danny kate bruce;
+#  do sudo useradd -m $i;
+# done
+# for i in management sales operations
+#  do sudo groupadd $i;
+# done
+# for i in management sales operations;
+#  do sudo usermod -a -G $i william;
+# done
+# for i in matt sam anne danny;
+#  do sudo usermod -a -G sales $i;
+# done
